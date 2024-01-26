d23e628fc6efc6cc3178231a1eac0526984b5c70
kirano-sharky <kieran.zhiming@gmail.com>
2024-01-26 20:33:00 +0800

    updated
    -router
    -login button (test)

--- next-auth-test/app/page.tsx
+++ next-auth-test/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { LoginButton } from "@/components/auth/login_button";
 import { MantineProvider, createTheme, Button, CopyButton } from "@mantine/core";
 import '@mantine/core/styles.css'
 import { IconPhoto, IconDownload, IconArrowRight } from '@tabler/icons-react';
@@ -10,28 +12,23 @@ export default function Home() {
       'ocean-blue': ['#7AD1DD', '#5FCCDB', '#44CADC', '#2AC9DE', '#1AC2D9', '#11B7CD', '#09ADC3', '#0E99AC', '#128797', '#147885'],
       'bright-pink': ['#F0BBDD', '#ED9BCF', '#EC7CC3', '#ED5DB8', '#F13EAF', '#F71FA7', '#FF00A1', '#E00890', '#C50E82', '#AD1374'],
     },
-    
+    //
   });
-
- return (
-  
+  console.log('ai which has 3IQ still better than Johnny')
+  return (
     <MantineProvider theme={theme}>
-      <main>
-        
-        <div className="space-y-6 text-center">
-        <h1 className='text-black'>
-          auth test
-        </h1>
-        <p className="text-black text-lg">
-          A simple authentication service
-        </p>
-        <Button leftSection={<IconPhoto size={14} />} size="md" color="black">hello world!</Button>
-       
-
-        
+      <main className="flex h-full flex-col items-center justify-center bg-slate-300">
+        <div className="space-y-4 text-center">
+          <h1 className="font-bold">next auth login</h1>
+          <p>this is a simple auth page</p>
+          <div>
+            <LoginButton >
+              <Button colors='ocean-blue' size="md" >Sign in</Button>
+            </LoginButton>
+          </div>
         </div>
       </main>
     </MantineProvider>
 
- )
+  )
 }
