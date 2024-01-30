e5fa6d668fa60c675c43bc1fc41e45c118a1f4b3
kirano-sharky <kieran.zhiming@gmail.com>
2024-01-30 22:19:50 +0800

    updated
    -login form (done)
    -invalid email & password

--- next-auth-test/components/auth/card_header.tsx
+++ next-auth-test/components/auth/card_header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 export const CardHeader = ({label}: HeaderProps) => {
     return (
         <div className="w-full flex flex-col gap-y-4 items-center justify-center">
-            <Text fw={700} size="xl">Next-Auth</Text>
+            <Text fw={700} size="25px">Next-Auth</Text>
             <p className="text-gray-500 relative bottom-3">{label}</p>
         </div>
     )
--- next-auth-test/components/auth/login_form.tsx
+++ next-auth-test/components/auth/login_form.tsx
@@ -1,17 +1,44 @@
 'use client'
-import { Card, Image, Text, Badge, Button, Group } from "@mantine/core"
+import { Button, TextInput, PasswordInput } from "@mantine/core"
 import { MantineProvider } from "@mantine/core"
 import '@mantine/core/styles.css'
 import { CardWrapper } from "./card_wrapper"
+import { IconAt } from '@tabler/icons-react';
+import { useForm } from '@mantine/form';
+import { useDisclosure } from '@mantine/hooks';
+
 
 export const LoginForm = () => {
+  //const [visible, { toggle }] = useDisclosure(false);
+  const form = useForm({
+    initialValues: {
+      email: '',
+      termsOfService: false,
+      password: ''
+    },
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email!'),
+      password: (value_1) => (/^\S+@\S+$/.test(value_1) ? null : 'Invalid password!'),
+    },
+  });
   return (
-    <div>
+    <div >
+      <MantineProvider>
+
       <CardWrapper headerLabel="Welcome to the page!" backButtonLabel="Don't have an account?" backButtonHref="/auth/register" showSocial>
-        <Text size="sm" c="dimmed" >
-          login form
-        </Text>
+        
+        <form className='' onSubmit={form.onSubmit((values) => console.log(values))}>
+          <TextInput label='Email' placeholder="Email" withAsterisk leftSection={<IconAt size={16} />} {...form.getInputProps('email')} radius='md' />
+        </form>
+        <form onSubmit={form.onSubmit((value_1) => console.log(value_1))}>
+          <PasswordInput label="Password" radius='md' withAsterisk placeholder='Password' {...form.getInputProps('password')}/>
+          <Button variant="light" mt='md' type="submit" fullWidth>Submit</Button>
+        </form>
+        
+
+       
       </CardWrapper>
+      </MantineProvider>
     </div>
   )
 }
