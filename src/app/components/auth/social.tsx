c53674bb72c16ad273e9ecf12733bd20fabb2f68
kirano-sharky <kieran.zhiming@gmail.com>
2024-05-13 23:52:28 +0800

    updated
    -OAuth (google & github) is in progress

--- src/app/components/auth/social.tsx
+++ src/app/components/auth/social.tsx
@@ -1,15 +1,22 @@
 'use client'
+import { signIn } from 'next-auth/react'
 import { Button } from '@/components/ui/button'
 import { FaGithub } from 'react-icons/fa'
 import { FcGoogle } from 'react-icons/fc'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
 
 export default function Social () {
+    const onClick = (provider: 'google' | 'github') => {
+        signIn(provider, {
+            callbackUrl: DEFAULT_LOGIN_REDIRECT
+        })
+    }
     return (
         <div className="flex items-center w-full gap-x-2">
-            <Button size={'lg'} className='w-full' variant={'outline'} onClick={() => {}}>
+            <Button size={'lg'} className='w-full' variant={'outline'} onClick={() => onClick('google')}>
                 <FcGoogle/>
             </Button>
-            <Button size={'lg'} className='w-full' variant={'outline'} onClick={() => {}}>
+            <Button size={'lg'} className='w-full' variant={'outline'} onClick={() => onClick('github')}>
                 <FaGithub/>
             </Button>
         </div>
